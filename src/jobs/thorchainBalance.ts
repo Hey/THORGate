34cb564acd8a1ca698e9fd3839184a434e1ed009
fodidjr38xj <fodidjr38xj@off.pm>
2024-04-10 17:08:04 +0200

    percentage

--- src/jobs/thorchainBalance.ts
+++ src/jobs/thorchainBalance.ts
@@ -2,14 +2,56 @@ import { findClosestTimeKey, redis } from "../redis";
 import { notifyBalanceChange } from "../notifications";
 import { Balance, fetchBalances } from "../thorchain";
 
-const walletNicknames = new Map([
-  ["thor1g98cy3n9mmjrpn0sxmn63lztelera37n8n67c0", "Pool Module"],
-  ["thor1dheycdevq39qlkxs2a6wuuzyn4aqxhve4qxtxt", "Reserve Module"],
-  ["thor17gw75axcnr8747pkanye45pnrwk7p9c3cqncsv", "Bond Module"],
-  ["thor1egxvam70a86jafa8gcg3kqfmfax3s0m2g3m754", "Treasury: LP"],
-  ["thor14n2q7tpemxcha8zc26j0g5pksx4x3a9xw9ryq9", "Treasury: 2"],
-  ["thor1qd4my7934h2sn5ag5eaqsde39va4ex2asz3yv5", "Treasury: 1"],
-  ["thor1wfe7hsuvup27lx04p5al4zlcnx6elsnyft7dzm", "Treasury: 3"],
+const wallets = new Map([
+  [
+    "thor1g98cy3n9mmjrpn0sxmn63lztelera37n8n67c0",
+    {
+      name: "Pool Module",
+      percentage: 5,
+    },
+  ],
+  [
+    "thor1dheycdevq39qlkxs2a6wuuzyn4aqxhve4qxtxt",
+    {
+      name: "Reserve Module",
+      percentage: 1,
+    },
+  ],
+  [
+    "thor17gw75axcnr8747pkanye45pnrwk7p9c3cqncsv",
+    {
+      name: "Bond Module",
+      percentage: 1,
+    },
+  ],
+  [
+    "thor1egxvam70a86jafa8gcg3kqfmfax3s0m2g3m754",
+    {
+      name: "Treasury: LP",
+      percentage: 1,
+    },
+  ],
+  [
+    "thor14n2q7tpemxcha8zc26j0g5pksx4x3a9xw9ryq9",
+    {
+      name: "Treasury: 2",
+      percentage: 1,
+    },
+  ],
+  [
+    "thor1qd4my7934h2sn5ag5eaqsde39va4ex2asz3yv5",
+    {
+      name: "Treasury: 1",
+      percentage: 1,
+    },
+  ],
+  [
+    "thor1wfe7hsuvup27lx04p5al4zlcnx6elsnyft7dzm",
+    {
+      name: "Treasury: 3",
+      percentage: 1,
+    },
+  ],
 ]);
 
 const compareAndAlert = async (
@@ -41,7 +83,7 @@ const compareAndAlert = async (
             await notifyBalanceChange(
               balance.denom,
               address,
-              walletNicknames.get(address) || address,
+              wallets.get(address)?.name ?? address,
               BigInt(historicalAmount),
               currentAmount,
               diffPercentage,
@@ -60,21 +102,15 @@ const compareAndAlert = async (
 };
 
 export const runThorchainBalanceJob = async () => {
-  for (const address of walletNicknames.keys()) {
-    console.log(
-      `Running balance check for ${walletNicknames.get(address) || address}...`,
-    );
+  for (const address of wallets.keys()) {
+    const name = wallets.get(address)?.name ?? address;
+    console.log(`Running balance check for ${name}...`);
     try {
       const currentBalances = await fetchBalances(address);
       await compareAndAlert(address, currentBalances);
-      console.log(
-        `Balance check complete for ${walletNicknames.get(address) || address}.`,
-      );
+      console.log(`Balance check complete for ${name}.`);
     } catch (error) {
-      console.error(
-        `Error in balance check for ${walletNicknames.get(address) || address}:`,
-        error,
-      );
+      console.error(`Error in balance check for ${name}:`, error);
     }
   }
 };
